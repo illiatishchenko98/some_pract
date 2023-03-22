5cfc04b0d778acc67118787ce7e9cb909b6aef64
Illia Tishchenko <102582375+illiatishchenko98@users.noreply.github.com>
2023-03-22 15:04:31 -0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,20 @@
+hello = 'Hello Python'
+
+
+personality = ' Illia is learning this awesome language'
+
+print(hello + personality + '!')
+
+
+name = 'Illia '
+
+print(name * 3)
+
+
+print(name.upper())
+
+print(name.lower())
+
+long_string = 'This is a huge sentance'
+
+print(long_string.split())
